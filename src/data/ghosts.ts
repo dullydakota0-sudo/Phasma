058229cfc82ef62606d72e58dbc22fd30e184ab4
dullydakota0-sudo <dullydakota0@gmail.com>
2025-08-25 15:16:15 -0500

    Add ghosts data

---
+++ src/data/ghosts.ts
@@ -0,0 +1,35 @@
+export const ghosts = [
+  {
+    name: "Banshee",
+    evidence: ["EMF 5", "Fingerprints", "Dots Projector"]
+  },
+  {
+    name: "Demon",
+    evidence: ["Freezing Temps", "Ghost Writing", "Fingerprints"]
+  },
+  {
+    name: "Jinn",
+    evidence: ["EMF 5", "Spirit Box", "Fingerprints"]
+  },
+  {
+    name: "Mare",
+    evidence: ["Spirit Box", "Ghost Writing", "Ghost Orb"]
+  },
+  {
+    name: "Moroi",
+    evidence: ["Spirit Box", "Ghost Writing", "Freezing Temps"]
+  },
+  {
+    name: "Myling",
+    evidence: ["EMF 5", "Ghost Writing", "Fingerprints"]
+  },
+  {
+    name: "Obake",
+    evidence: ["EMF 5", "Fingerprints", "Ghost Orb"]
+  },
+  {
+    name: "Onryo",
+    evidence: ["Spirit Box", "Freezing Temps", "Ghost Orb"]
+  }
+  // Add more ghosts as per latest patch notes!
+];
